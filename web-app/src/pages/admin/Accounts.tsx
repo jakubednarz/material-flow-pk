dcb34b0c018ad41743a5939df952cb6a71997fad
Jakub Bednarz <bednaszjakub@gmail.com>
2025-03-21 19:27:39 +0100

    feat(ui): add Admin's Accouts page

--- web-app/src/pages/admin/Accounts.tsx
+++ web-app/src/pages/admin/Accounts.tsx
@@ -1,10 +1,41 @@
 import React from "react";
+import { Typography } from "@mui/material";
+import Section from "../../components/Section";
+import CreateUserSection from "../../features/admin/CreateUserSection";
+import ListUsersSection from "../../features/admin/ListUsersSection";
 
 const Accounts: React.FC = () => {
   return (
-    <div>
-      <h1>Accounts</h1>
-      <p>Welcome to Accounts.</p>
+    <div className="w-full h-full">
+      <div className="flex w-full p-4 gap-4 ">
+        <div className="w-8/12 flex flex-col gap-4">
+          <div className="relative flex gap-4">
+            <Section title="Section title">
+              <Typography variant="h5" className="">
+                Additional Section
+              </Typography>
+            </Section>
+
+            <Section title="Section title">
+              <Typography variant="h5" className="">
+                Additional Section
+              </Typography>
+            </Section>
+          </div>
+
+          <ListUsersSection />
+        </div>
+
+        <div className="w-5/12 flex-col space-y-4">
+          <CreateUserSection />
+
+          <Section title="Section title">
+            <Typography variant="h5" className="">
+              Additional Section
+            </Typography>
+          </Section>
+        </div>
+      </div>
     </div>
   );
 };
