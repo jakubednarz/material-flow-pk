2bdf2191ca0aeda467afe83b5867850e216992bb
Jakub Bednarz <bednaszjakub@gmail.com>
2025-04-03 14:43:20 +0200

    feat: add orders routes

--- web-app/src/pages/admin/Dashboard.tsx
+++ web-app/src/pages/admin/Dashboard.tsx
@@ -15,6 +15,15 @@ import MonitoringAnalyticsPage from "./MonitoringAnalyticsPage";
 import InventoryRoundedIcon from "@mui/icons-material/InventoryRounded";
 import MoveDownRoundedIcon from "@mui/icons-material/MoveDownRounded";
 import BarChartRoundedIcon from "@mui/icons-material/BarChartRounded";
+import CategoryRoundedIcon from "@mui/icons-material/CategoryRounded";
+import LocalShippingRoundedIcon from "@mui/icons-material/LocalShippingRounded";
+import InternalOrdersPage from "./InternalOrdersPage";
+import SuppliersManagementPage from "./SuppliersManagementPage";
+import ReportsAnalyticsPage from "./ReportsAnalyticsPage";
+import OrdersHistoryPage from "./OrdersHistoryPage";
+import ExternalOrdersPage from "./ExternalOrdersPage";
+import LaunchRoundedIcon from "@mui/icons-material/LaunchRounded";
+import { Icon } from "@mui/material";
 
 const AdminDashboard: React.FC = () => {
   const menuItems = [
@@ -33,38 +42,77 @@ const AdminDashboard: React.FC = () => {
       icon: <PersonIcon />,
     },
     {
-      id: 4,
+      id: 3,
       path: "/accounts",
       group: "Human resources",
       name: "Accounts",
       icon: <GroupRoundedIcon />,
     },
     {
-      id: 5,
+      id: 4,
       path: "/activity-history",
       group: "Human resources",
       name: "Activity History",
       icon: <HistoryRoundedIcon />,
     },
     {
-      id: 6,
+      id: 5,
       path: "/inventory-management",
       group: "Warehouse",
       name: "Inventory Management",
-      icon: <InventoryRoundedIcon />,
+      icon: <CategoryRoundedIcon />,
     },
     {
-      id: 7,
+      id: 6,
       path: "/warehouse-operations",
       group: "Warehouse",
       name: "Warehouse Operations",
       icon: <MoveDownRoundedIcon />,
     },
     {
-      id: 8,
+      id: 7,
       path: "/monitoring-analytics",
       group: "Warehouse",
       name: "Monitoring & Analytics",
+      icon: <BarChartRoundedIcon />,
+    },
+    {
+      id: 8,
+      path: "/internal-orders",
+      group: "Orders Management",
+      name: "Internal Orders",
+      icon: (
+        <Icon>
+          <img src="/internal-link.png" alt="" />
+        </Icon>
+      ),
+    },
+    {
+      id: 9,
+      path: "/external-orders",
+      group: "Orders Management",
+      name: "External Orders",
+      icon: <LaunchRoundedIcon />,
+    },
+    {
+      id: 10,
+      path: "/orders-history",
+      group: "Orders",
+      name: "Orders History",
+      icon: <HistoryRoundedIcon />,
+    },
+    {
+      id: 11,
+      path: "/suppliers-management",
+      group: "Orders",
+      name: "Suppliers Management",
+      icon: <LocalShippingRoundedIcon />,
+    },
+    {
+      id: 12,
+      path: "/reports-analytics",
+      group: "Orders",
+      name: "Reports & Analytics",
       icon: <BarChartRoundedIcon />,
     },
   ];
@@ -90,6 +138,17 @@ const AdminDashboard: React.FC = () => {
             <Route
               path="/monitoring-analytics"
               element={<MonitoringAnalyticsPage />}
+            />
+            <Route path="/internal-orders" element={<InternalOrdersPage />} />
+            <Route path="/external-orders" element={<ExternalOrdersPage />} />
+            <Route path="/orders-history" element={<OrdersHistoryPage />} />
+            <Route
+              path="/suppliers-management"
+              element={<SuppliersManagementPage />}
+            />
+            <Route
+              path="/reports-analytics"
+              element={<ReportsAnalyticsPage />}
             />
             <Route path="*" element={<Navigate to="/" />} />
           </Routes>
