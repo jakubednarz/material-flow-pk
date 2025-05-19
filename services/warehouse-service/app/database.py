e13cf7a80a9464be6b36c1534d91c10ccf786d8f
Jakub Bednarz <bednaszjakub@gmail.com>
2025-05-19 16:32:58 +0200

    feat(warehouse-service): add cascade del to db

--- services/warehouse-service/app/database.py
+++ services/warehouse-service/app/database.py
@@ -3,6 +3,9 @@ from typing import Annotated
 
 from dotenv import find_dotenv
 from fastapi import Depends
+
+# from sqlalchemy.ext.compiler import compiles
+# from sqlalchemy.schema import DropTable
 from sqlmodel import Session, SQLModel, create_engine
 
 from .models.warehouse import *  # noqa: F403
@@ -11,6 +14,11 @@ find_dotenv()
 USER_DATABASE_URL = os.getenv("WAREHOUSE_DB_URL")
 
 engine = create_engine(USER_DATABASE_URL)
+
+
+# @compiles(DropTable)
+# def _compile_drop_table(element, compiler, **kwargs):
+#     return compiler.visit_drop_table(element) + " CASCADE"
 
 
 def create_db_and_tables():
